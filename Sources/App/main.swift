a0319ab327883a946586ae44496722fdac4f2ad7
Kyle McMahon <kyle.mcmahon@mac.com>
2016-10-25 22:27:37 -0400

    Fix templating

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -6,7 +6,7 @@ drop.get(String.self) { request, team in
     return try drop.view.make("index", [
         "lat": "41.496235",
         "lon": "-81.684773",
-        "name": "Progressive Field",
+        "park": "Progressive Field",
         "color": "002B5C"
     ])
 }
