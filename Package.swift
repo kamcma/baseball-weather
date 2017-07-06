1966222c037c49d3c0fb7753909cad50e4d97f91
Kyle McMahon <kyle.mcmahon@mac.com>
2017-07-06 18:19:31 -0400

    Conform to Vapor template structure for testing purposes

--- Package.swift
+++ Package.swift
@@ -2,7 +2,14 @@ import PackageDescription
 
 let package = Package(
     name: "baseball-weather",
+    targets: [
+        Target(name: "App"),
+        Target(name: "Run", dependencies: ["App"])
+    ],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
+    ],
+    exclude: [
+        "Config"
     ]
 )
---
+++ Sources/App/Config+Setup.swift
@@ -0,0 +1,5 @@
+extension Config {
+    public func setup() throws {
+
+    }
+}
