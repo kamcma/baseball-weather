7c77c417cd72566bc4d845764c816b4c4f46fcd5
Kyle McMahon <kyle.mcmahon@mac.com>
2017-10-13 19:26:23 -0400

    Update to Swift 4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,23 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "baseball-weather",
-    targets: [
-        Target(name: "App"),
-        Target(name: "Run", dependencies: ["App"])
+    products: [
+        .library(name: "App", targets: ["App"]),
+        .executable(name: "Run", targets: ["Run"])
     ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
+        .package(url: "https://github.com/vapor/vapor.git", from: "2.2.2")
     ],
-    exclude: [
-        "Config"
+    targets: [
+        .target(name: "App", dependencies: ["Vapor"],
+            exclude: [
+                "Config",
+                "Resources"
+            ]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
     ]
 )
