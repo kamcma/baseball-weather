59dc747373bf57bb7248931e2a9e7dc43c87b9eb
Kyle McMahon <kyle.mcmahon@mac.com>
2018-04-28 21:06:15 -0400

    Update routing

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -2,21 +2,13 @@
 import Vapor
 import MLB
 
-final class Routes: RouteCollection {
-    let app: Application
-
-    init(app: Application) {
-        self.app = app
+public func routes(_ router: Router) throws {
+    router.get(Team.parameter) { req -> Response in
+        let coordinates = try req.parameters.next(Team.self).parkCoordinates
+        return req.redirect(to: "https://darksky.net/forecast/\(coordinates.latitude),\(coordinates.longitude)/")
     }
 
-    func boot(router: Router) throws {
-        router.get(Team.parameter) { req -> Response in
-            let coordinates = try req.parameter(Team.self).parkCoordinates
-            return req.redirect(to: "https://darksky.net/forecast/\(coordinates.latitude),\(coordinates.longitude)/")
-        }
-
-        router.get { req -> Future<View> in
-            return try req.view().render("index.html")
-        }
+    router.get { req -> Future<View> in
+        return try req.view().render("index.html")
     }
 }
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,5 +5,7 @@ public func configure(
     _ env: inout Environment,
     _ services: inout Services
 ) throws {
-
+    let router = EngineRouter.default()
+    try routes(router)
+    services.register(router, as: Router.self)
 }
